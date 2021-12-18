1a708683ab17ae4a067f8055a08033cd8c126e0d
Andrey <s2-a@yandex.ru>
2021-12-19 00:14:32 +0500

    moves frontend 1.10

--- src/components/WrongPath/WrongPath.js
+++ src/components/WrongPath/WrongPath.js
@@ -4,10 +4,10 @@ import './WrongPath.css';
 
 const WrongPath = () => (
   <>
-    <section className="not-found">
-      <h1 className="not-found__title">404</h1>
-      <p className="not-found__subtitle">Страница не найдена</p>
-      <Link to="/" className="not-found__back-link">Назад</Link>
+    <section className="wrong-path">
+      <h1 className="wrong-path__title">404</h1>
+      <p className="wrong-path__subtitle">Страница не найдена</p>
+      <Link to="/" className="wrong-path__back-link">Назад</Link>
     </section>
   </>
 );
