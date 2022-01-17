6bbfa0c34aed2cf5271bee5582d2ad9fd96f0ea5
Andrey <s2-a@yandex.ru>
2022-01-17 09:42:17 +0500

    moves frontend 3.30

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,9 +4,9 @@ export const BASE_URL   = "https://api.nomoreparties.co/beatfilm-movies";
 /* export const MAIN_API   = "http://localhost:4000/";
 export const AUTH       = "http://localhost:4000"; */
 
-export const MAIN_API   = "https://domainname1f.nomoredomains.rocks/";
-export const AUTH       = "https://domainname1f.nomoredomains.rocks";
+export const MAIN_API   = "https://domainname1b.nomoredomains.rocks/";
+export const AUTH       = "https://domainname1b.nomoredomains.rocks";
 
-export const MAX_SHORT_MOVIE_DORATION = 40;
-export const MIN_NUMBER_OF_CARDS = 4;
+export const MIN_NUMBER_OF_CARDS = 12;
 export const MAX_NUMBER_OF_CARDS = 100;
+export const MAX_SHORT_MOVIE_DORATION = 40;
